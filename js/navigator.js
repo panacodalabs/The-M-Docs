5c771811ee8a2f0e8b915f223b8ae7dc1a13f853
Dominik <mail@dominik-laubach.de>
2011-06-10 17:50:49 +0200

    updated navigation

--- js/navigator.js
+++ js/navigator.js
@@ -71,7 +71,7 @@ $('document').ready(function() {
         }
 
         history.back(-1);
-        loadArticle(window.location.hash, false, true);
+        loadArticle(window.location.hash, true, true);
     });
 
 });
