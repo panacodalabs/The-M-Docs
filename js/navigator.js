30fe60a7f30d903953e3de7c9481f3fc801d527c
Dominik <mail@dominik-laubach.de>
2011-06-15 15:54:25 +0200

    updated search result page

--- js/navigator.js
+++ js/navigator.js
@@ -117,6 +117,7 @@ function generateIndex() {
                                 tmp = tmp.replace(/<\/span>/g, '');
                                 tmp = tmp.replace(/<\/p>/g, '');
                                 tmp = tmp.replace(/<\/pre>/g, '');
+                                alert(tmp);
                                 content += tmp;
                             }
                         }
