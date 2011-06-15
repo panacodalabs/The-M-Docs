7d90cea6195324acea42188b319321b89ada60af
Dominik <mail@dominik-laubach.de>
2011-06-15 13:38:06 +0200

    updated search result page

--- js/navigator.js
+++ js/navigator.js
@@ -151,6 +151,8 @@ function filterResult(searchString, result, position) {
     var start = position - 75 >= 0 ? position - 75 : 0;
     var end = position + position.length + (start === 0 ? 150 : 75) >= result.length ? result.length : position + position.length + (start === 0 ? 150 : 75);
     result = (start > 0 ? '... ' : '') + result.substring(start, end) + (end < result.length ? ' ...' : '');
+    console.log(start);
+    console.log(end);
     //result = result.replace(new RegExp(searchString, 'g'), '<span class="highlight">' + searchString + '</span>');
     return result;
 }
